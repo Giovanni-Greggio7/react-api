0c920fffc84af951c6f3985428ae89687029b7d4
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-02-28 16:58:05 +0100

    Exercise over

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const endpoint = 'http://localhost:3000/posts.js/'
+
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = () => {
+    axios
+      .get(endpoint)
+      .then((res) => setPosts(res.data))
+      .catch(err => console.error(err))
+  }
+
+  useEffect(fetchPosts, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <h1>React Post</h1>
+        <div className="table-responsive">
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">title</th>
+                <th scope="col">image</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.length > 0 ? (
+                posts.map((elem) => {
+                  return (
+                    <tr key={elem.id}>
+                      <td>{elem.id}</td>
+                      <td>{elem.title}</td>
+                      <td><img src={elem.image} width={'200px'} alt="" srcset="" /></td>
+                    </tr>
+                  )
+                })
+              ) : (
+                <tr>
+                  <td>
+                    <h3>Non ci sono posts</h3>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
